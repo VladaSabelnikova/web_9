f216a887fa6a1301baea797755c41870d2b49be4
vlada <vladasabelnikova@gmail.com>
2022-02-26 21:47:41 +0300

    Задача 2 решена
    Готово.

---
+++ task_2.py
@@ -0,0 +1,46 @@
+class Jobs(SqlAlchemyBase):
+    __tablename__ = 'jobs'
+
+    def __str__(self):
+        return f'{self.team_leader} {self.job} {self.work_size}' \
+               f' {self.collaborators} {self.is_finished}'
+
+    id = sqlalchemy.Column(
+        sqlalchemy.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    job = sqlalchemy.Column(
+        sqlalchemy.String,
+        nullable=True
+    )
+
+    work_size = sqlalchemy.Column(
+        sqlalchemy.Integer,
+        nullable=True
+    )
+
+    collaborators = sqlalchemy.Column(
+        sqlalchemy.String,
+        nullable=True
+    )
+
+    start_date = sqlalchemy.Column(
+        sqlalchemy.DateTime,
+        nullable=True,
+        default=datetime.datetime.now
+    )
+    end_date = sqlalchemy.Column(
+        sqlalchemy.DateTime
+    )
+
+    is_finished = sqlalchemy.Column(
+        sqlalchemy.Boolean
+    )
+
+    team_leader = user_id = sqlalchemy.Column(
+        sqlalchemy.Integer,
+        sqlalchemy.ForeignKey("users.id")
+    )
+    user = orm.relation('User')
